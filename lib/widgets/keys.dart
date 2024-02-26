7e58df44f9b1499060268269553877593f882af4
DanielVieiraSS <danielvieira2828@gmail.com>
2024-02-26 10:37:16 -0300

    .

--- lib/widgets/keys.dart
+++ lib/widgets/keys.dart
@@ -72,7 +72,7 @@ class _KeysState extends State<Keys> {
                   // for (final todo in _orderedTodos) TodoItem(todo.text, todo.priority),
                   for (final todo in _orderedTodos)
                     CheckableTodoItem(
-                      key: ValueKey(),
+                      key: ValueKey(todo.text),
                       todo.text,
                       todo.priority,
                     ),
